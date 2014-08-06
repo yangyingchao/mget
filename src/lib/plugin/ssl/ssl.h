eb16c446ce11ec35c5ff0eb858c20e43561ba53a
yangyingchao <yangyingchao@gmail.com>
2014-08-06 22:37:44 +0800

    fixed building on Linux.

--- src/lib/plugin/ssl/ssl.h
+++ src/lib/plugin/ssl/ssl.h
@@ -24,11 +24,11 @@
 #ifndef _SSL_H_
 #define _SSL_H_
 
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-
 #include "mget_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 bool ssl_init();
 
@@ -37,9 +37,9 @@ uint32 secure_socket_read(int sk, char *buf, uint32 size, void *priv);
 
 uint32 secure_socket_write(int sk, char *buf, uint32 size, void *priv);
 
-// #ifdef __cplusplus
-// }
-// #endif
+#ifdef __cplusplus
+}
+#endif
 
 #endif				/* _SSL_H_ */
 
