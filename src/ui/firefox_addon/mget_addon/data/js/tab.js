3b56f257dc108bc3bc1d583969b09e384c0467e8
yangyingchao <yangyingchao@gmail.com>
2014-10-16 21:43:36 +0800

    auto

--- src/ui/firefox_addon/mget_addon/data/js/tab.js
+++ src/ui/firefox_addon/mget_addon/data/js/tab.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
         timedCount();
     });
 
-
-    // window.setInterval(timedCount, 2000);
-
+    $("#homepage").click(function(){
+        alert("TODO: should open homepage at new tab!!");
+    });
 });
