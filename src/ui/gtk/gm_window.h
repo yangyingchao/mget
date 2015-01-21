ac2843f206f84ee0f58e761e8cb0a37a4e14225b
yangyingchao <yangyingchao@gmail.com>
2015-01-21 22:24:40 +0800

    added gtk.

---
+++ src/ui/gtk/gm_window.h
@@ -0,0 +1,101 @@
+/** gm_window.h --- main window class
+ *
+ * Copyright (C) 2014 Yang,Ying-chao
+ *
+ * Author: Yang,Ying-chao <yangyingchao@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _GM_WINDOW_H_
+#define _GM_WINDOW_H_
+
+#include <gtk/gtk.h>
+#include "../../lib/logutils.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GM_TYPE_WINDOW         (gm_window_get_type ())
+#define GM_WINDOW(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GM_TYPE_WINDOW, GmWindow))
+#define GM_WINDOW_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST ((k), GM_TYPE_WINDOW, GmWindowClass))
+#define GM_IS_WINDOW(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GM_TYPE_WINDOW))
+#define GM_IS_WINDOW_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GM_TYPE_WINDOW))
+#define GM_WINDOW_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GM_TYPE_WINDOW, GmWindowClass))
+
+typedef struct _GmWindow       GmWindow;
+typedef struct _GmWindowClass  GmWindowClass;
+typedef struct _GmWindowPriv   GmWindowPriv;
+typedef struct _GmApp GmApp;
+
+#define G_GET_WIDGET(B, T, N) (T*)(gtk_builder_get_object((B), N))
+
+#define GD_PTR(T, v, p) T* v = (T*)(p)
+
+typedef enum
+{
+    GM_UPDATE_PROGRESS_NEW_WINDOW = 1 << 0,
+    GM_UPDATE_PROGRESS_NEW_TAB    = 1 << 1
+} GmOpenLinkFlags;
+
+struct _GmWindow {
+    GtkApplicationWindow parent_instance;
+    GmWindowPriv *priv;
+};
+
+typedef enum _task_status
+{
+    TS_CREATED = 0,
+    TS_STARTED,
+    TS_PAUSED,
+    TS_FINISHED,
+    TS_FAILED,
+    TS_INVALID,
+} task_status;
+
+
+struct _GmWindowClass {
+    GtkApplicationWindowClass parent_class;
+
+    /* Signals */
+    void (*update_progress) (void*       window,
+                             const char* name,
+                             const char* size,
+                             double      percentage,
+                             const char* speed,
+                             const char* eta,
+                             gpointer    user_data);
+
+    void (*status_changed) (void* window,
+                           const char* name,
+                           task_status stat,
+                           const char* msg,
+                           gpointer   user_data);
+};
+
+GType      gm_window_get_type     (void) G_GNUC_CONST;
+GtkWidget *gm_window_new          (GmApp       *application);
+void       gm_window_search       (GmWindow    *window,
+                                   const gchar *str);
+void       _gm_window_display_uri (GmWindow    *window,
+                                   const gchar *uri);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _GM_WINDOW_H_ */
---
+++ src/ui/gtk/gmget.c
@@ -0,0 +1,176 @@
+#include <gtk/gtk.h>
+#include <libmget/libmget.h>
+#include <libmget/mget_macros.h>
+#include "mget_thread.h"
+#include <stdlib.h>
+#include <string.h>
+
+static GtkBuilder* g_builder   = NULL;
+
+
+static int nc = 10;
+
+#define show_iter(X)       printf("ITER: %u, %p, %p, %p\n", \
+                                  (X)->stamp, (X)->user_data, \
+                                  (X)->user_data2,            \
+                                  (X)->user_data3)
+
+
+void update_progress(metadata* md)
+{
+    static int    idx       = 0;
+    static uint32 ts        = 0;
+    static uint64 last_recv = 0;
+
+    if (md->hd.status == RS_SUCCEEDED) {
+        char *date = current_time_str();
+
+        printf("%s - %s saved in %s [%.02fKB/s] ...\n",
+               date, md->fn,
+               stringify_time(md->hd.acc_time),
+               (double) (md->hd.package_size) / K / md->hd.acc_time);
+        free(date);
+        return;
+    }
+
+    int threshhold = 78 * md->hd.acon / md->hd.nr_effective;
+    if (idx < threshhold) {
+        if (!ts) {
+            ts = get_time_ms();
+            fprintf(stderr, ".");
+        } else if ((get_time_ms() - ts) > 1000 / threshhold * idx) {
+            fprintf(stderr, ".");
+            idx++;
+        }
+    } else {
+        data_chunk *dp    = md->body;
+        uint64      total = md->hd.package_size;
+        uint64      recv  = 0;
+
+        for (int i = 0; i < md->hd.nr_effective; ++i) {
+            recv += dp->cur_pos - dp->start_pos;
+            dp++;
+        }
+
+        uint64 diff_size = recv - last_recv;
+        uint64 remain    = total - recv;
+        uint32 c_time    = get_time_ms();
+        uint64 bps       = (uint64)((double) (diff_size) * 1000 / (c_time - ts));
+
+        fprintf(stderr,
+                "] %.02f percent finished, speed: %s/s, eta: %s\r",
+                (double) recv / total * 100,
+                stringify_size(bps),
+                stringify_time((total-recv)/bps));
+        fprintf(stderr, "\n");
+
+        idx       = 0;
+        last_recv = recv;
+        ts        = c_time;
+    }
+}
+
+#define G_GET_WIDGET(T, N)   \
+    (T*)(gtk_builder_get_object(g_builder, N))
+
+gboolean  on_MainWindow_destroy(GtkWidget *widget,
+                                GdkEvent  *event,
+                                gpointer   user_data)
+{
+    gtk_widget_destroy(widget);
+    gtk_main_quit();
+ }
+
+void on_btn_add_clicked(GtkButton *button,
+                        gpointer   user_data)
+{
+    printf ("hello!\n");
+    GtkWidget* dlg = (GtkWidget*)user_data;
+    gtk_widget_show_all (dlg);
+}
+
+void on_btn_download_cancel_clicked(GtkButton *button,
+                                    gpointer   user_data)
+{
+    GtkWidget* dlg = (GtkWidget*)user_data;
+    gtk_widget_hide (dlg);
+}
+
+void on_btn_download_confirm_clicked(GtkButton *button,
+                                     gpointer   user_data)
+{
+    GtkWidget* dlg = (GtkWidget*)user_data;
+
+    GtkEntry* entry_url = G_GET_WIDGET(GtkEntry, "entry_url");
+    const gchar* url = gtk_entry_get_text(entry_url);
+    if (!url || !strlen(url))
+    {
+        goto out;
+    }
+
+    gmet_request* g_request = ZALLOC1(gmet_request);
+    GtkEntry* entry_dir = G_GET_WIDGET(GtkEntry, "entry_dir");
+    const gchar* dir = gtk_entry_get_text(entry_dir);
+    if (dir && strlen(dir))
+    {
+        g_request->request.fn.dirn = g_strdup(dir);
+    }
+
+    GtkEntry* entry_fn = G_GET_WIDGET(GtkEntry, "entry_filename");
+    const gchar* fn = gtk_entry_get_text(entry_fn);
+    if (fn && strlen(fn))
+    {
+        g_request->request.fn.basen = g_strdup(fn);
+    }
+
+    printf ("url: %s, dir: %s, fn: %s\n", url, dir, fn);
+
+    bool v = false;
+
+    GtkListStore* store_tasks = G_GET_WIDGET(GtkListStore, "liststore_tasks");
+    printf ("soter: %p\n", store_tasks);
+    GtkTreeIter* iter = &(g_request->iter);
+    gtk_list_store_append(store_tasks, iter);
+    show_iter(iter);
+    gtk_list_store_set(store_tasks, iter, 1, url, 2, "Unkown",
+                       3, (double)0, -1);
+    /* int r = start_request(url, &g_request->request.fn, nc, update_progress, &v); */
+    /* printf ("r = %d\n", r); */
+
+    //@todo:
+    // 1. Make hash table to store request.
+    // 2. Start threads to handle g_request.
+    
+
+out:
+    gtk_widget_hide (dlg);
+}
+
+int main(int argc, char *argv[])
+{
+    gtk_init(&argc, &argv);
+    g_builder = gtk_builder_new();
+
+    guint ret = gtk_builder_add_from_file(g_builder, "res/gmget.glade", NULL);
+    if (!ret)
+    {
+        printf ("Failed to parse glade file!\n");
+        return 1;
+    }
+
+    GtkWidget* window = G_GET_WIDGET(GtkWidget, "MainWindow");
+    if (!window)
+    {
+        printf ("Failed to get window!\n");
+        return -1;
+    }
+
+    /* g_signal_connect (window, "destroy", G_CALLBACK (on_window_destroy), NULL); */
+    gtk_builder_connect_signals(g_builder, NULL);
+    /* g_object_unref(g_builder); */
+
+    gtk_widget_show(window);
+
+    gtk_main();
+    return 0;
+}
